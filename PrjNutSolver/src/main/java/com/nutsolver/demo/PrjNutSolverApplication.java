afd3594588e70ccfb35d5efaf0d89167b67330b6
GuilhermeLealDev <guilherme.leal.dev2025@gmail.com>
2026-02-18 14:18:46 -0300

    Esqueleto do projeto
    Application propeties configurada para conectar em db_NutSolver

---
+++ PrjNutSolver/src/main/java/com/nutsolver/demo/PrjNutSolverApplication.java
@@ -0,0 +1,13 @@
+package com.nutsolver.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjNutSolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjNutSolverApplication.class, args);
+	}
+
+}
